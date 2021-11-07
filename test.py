9dbdd55e7d8cf3cc26f5fc15360db6931c0be36c
Josh Anderson <josh.anderson@contrastsecurity.com>
2021-11-07 19:08:54 +0000

    Add unit tests

---
+++ test.py
@@ -0,0 +1,364 @@
+import contextlib
+import io
+import unittest
+from re import match
+from unittest.mock import patch
+
+import responses
+from actions_toolkit.core import ExitCode
+from responses import matchers
+
+from verify import ContrastVerifyAction
+
+
+class ActionTestCase(unittest.TestCase):
+    def setUp(self):
+        self._header_matcher = matchers.header_matcher(
+            {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Api-Key": "An_Api_Key",
+                "Authorization": "Base64Header",
+            }
+        )
+
+        responses.add(
+            responses.GET,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/applications/name?filterText=VerifierTest",
+            json={
+                "applications": [
+                    {"name": "VerifierTest", "app_id": "verifier_app_uuid"}
+                ]
+            },
+        )
+
+        self._action = ContrastVerifyAction(
+            {
+                "APP_NAME": "VerifierTest",
+                "BASE_URL": "https://apptwo.contrastsecurity.com/api/ng/anOrgId/",
+                "CONTRAST_API_KEY": "An_Api_Key",
+                "CONTRAST_AUTHORIZATION": "Base64Header",
+                "BUILD_NUMBER": "123",
+                "FAIL_THRESHOLD": 0,
+                "SEVERITIES": "HIGH,CRITICAL",
+            }
+        )
+
+    @responses.activate
+    def test_validate_connection_valid(self):
+        responses.add(
+            responses.GET,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/profile/",
+            json={},
+            status=200,
+            match=[self._header_matcher],
+        )
+
+        # it should succeed
+        self._action.validate_connection()
+
+    @responses.activate
+    def test_validate_connection_invalid(self):
+        responses.add(
+            responses.GET,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/profile/",
+            status=403,
+            match=[self._header_matcher],
+        )
+
+        out = io.StringIO()
+        # it should quit
+        with self.assertRaises(SystemExit) as cm:
+            with contextlib.redirect_stdout(out):
+                self._action.validate_connection()
+        # it should exit non-zero
+        self.assertEqual(cm.exception.code, 1)
+        # it should log a useful message
+        self.assertIn(
+            "Connection test failed, please verify credentials (agent credentials will not work) -",
+            out.getvalue(),
+        )
+
+    @responses.activate
+    def test_determine_application_id_by_name_found(self):
+        out = io.StringIO()
+        with contextlib.redirect_stdout(out):
+            self._action.determine_application_id()
+        # it should log a useful message
+        self.assertIn(
+            'Application ID for "VerifierTest" is verifier_app_uuid',
+            out.getvalue(),
+        )
+
+    @responses.activate
+    def test_determine_application_id_by_name_not_found(self):
+        responses.add(
+            responses.GET,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/applications/name?filterText=NonExistentApp",
+            json={"applications": [{"name": "NonExactMatch", "app_id": "uuid"}]},
+            match=[self._header_matcher],
+        )
+
+        self._action = ContrastVerifyAction(
+            {
+                "APP_NAME": "NonExistentApp",
+                "BASE_URL": "https://apptwo.contrastsecurity.com/api/ng/anOrgId/",
+                "CONTRAST_API_KEY": "An_Api_Key",
+                "CONTRAST_AUTHORIZATION": "Base64Header",
+                "BUILD_NUMBER": "123",
+                "FAIL_THRESHOLD": 0,
+                "SEVERITIES": "HIGH,CRITICAL",
+            }
+        )
+
+        out = io.StringIO()
+        # it should quit
+        with self.assertRaises(SystemExit) as cm:
+            with contextlib.redirect_stdout(out):
+                self._action.determine_application_id()
+        # it should exit non-zero
+        self.assertEqual(cm.exception.code, 1)
+        # it should log a useful message
+        self.assertIn(
+            'Could not match one app with name "NonExistentApp", found 0, consider using APP_ID input instead.',
+            out.getvalue(),
+        )
+
+    @responses.activate
+    def test_verify_application_with_succeeding_job_outcome_policy(self):
+        responses.add(
+            responses.POST,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/securityChecks",
+            status=200,
+            json={"security_check": {"result": True}},
+            match=[
+                self._header_matcher,
+                matchers.json_params_matcher(
+                    {
+                        "application_id": "verifier_app_uuid",
+                        "security_check_filter": {
+                            "app_version_tags": ["123"],
+                            "query_by": "APP_VERSION_TAG",
+                        },
+                        "origin": "GitHub/Python",
+                    }
+                ),
+            ],
+        )
+
+        out = io.StringIO()
+        with contextlib.redirect_stdout(out):
+            self._action.verify_application()
+        # it should log a useful message
+        self.assertIn(
+            "Step passes matching policy",
+            out.getvalue(),
+        )
+
+    @responses.activate
+    def test_verify_application_with_failing_job_outcome_policy(self):
+        responses.add(
+            responses.POST,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/securityChecks",
+            status=200,
+            json={
+                "security_check": {
+                    "result": False,
+                    "job_outcome_policy": {
+                        "outcome": "FAILED",
+                        "name": "Test Job Outcome Policy",
+                        "opt_into_query": True,
+                    },
+                }
+            },
+            match=[
+                self._header_matcher,
+                matchers.json_params_matcher(
+                    {
+                        "application_id": "verifier_app_uuid",
+                        "security_check_filter": {
+                            "app_version_tags": ["123"],
+                            "query_by": "APP_VERSION_TAG",
+                        },
+                        "origin": "GitHub/Python",
+                    }
+                ),
+            ],
+        )
+
+        out = io.StringIO()
+        # it should quit
+        with self.assertRaises(SystemExit) as cm:
+            with contextlib.redirect_stdout(out):
+                self._action.verify_application()
+        # it should exit non-zero
+        self.assertEqual(cm.exception.code, 1)
+        # it should log a useful message
+        self.assertIn(
+            'Contrast verify gate fails with status FAILED - policy "Test Job Outcome Policy"',
+            out.getvalue(),
+        )
+
+    @responses.activate
+    def test_verify_application_with_failing_job_outcome_policy_ignoring_build_version(
+        self,
+    ):
+        responses.add(
+            responses.POST,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/securityChecks",
+            status=200,
+            json={
+                "security_check": {
+                    "result": False,
+                    "job_outcome_policy": {
+                        "outcome": "FAILED",
+                        "name": "Test Job Outcome Policy",
+                        "opt_into_query": False,
+                    },
+                }
+            },
+            match=[
+                self._header_matcher,
+                matchers.json_params_matcher(
+                    {
+                        "application_id": "verifier_app_uuid",
+                        "security_check_filter": {
+                            "app_version_tags": ["123"],
+                            "query_by": "APP_VERSION_TAG",
+                        },
+                        "origin": "GitHub/Python",
+                    }
+                ),
+            ],
+        )
+
+        out = io.StringIO()
+        # it should quit
+        with self.assertRaises(SystemExit) as cm:
+            with contextlib.redirect_stdout(out):
+                self._action.verify_application()
+        # it should exit non-zero
+        self.assertEqual(cm.exception.code, 1)
+        # it should warn that all vulnerabilities are being considered
+        self.assertIn(
+            'Matching policy "Test Job Outcome Policy" is not configured to apply the "query vulnerabilities by selection from the plugin when filtering vulnerabilities" option, this means all open vulnerabilities will be considered, not just those from the build_number input.',
+            out.getvalue(),
+        )
+        # it should log a useful message
+        self.assertIn(
+            'Contrast verify gate fails with status FAILED - policy "Test Job Outcome Policy"',
+            out.getvalue(),
+        )
+
+    @responses.activate
+    def test_verify_application_with_no_job_outcome_policy_below_threshold(
+        self,
+    ):
+        responses.add(
+            responses.POST,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/securityChecks",
+            status=200,
+            json={"security_check": {"result": None}},
+            match=[
+                self._header_matcher,
+                matchers.json_params_matcher(
+                    {
+                        "application_id": "verifier_app_uuid",
+                        "security_check_filter": {
+                            "app_version_tags": ["123"],
+                            "query_by": "APP_VERSION_TAG",
+                        },
+                        "origin": "GitHub/Python",
+                    }
+                ),
+            ],
+        )
+
+        responses.add(
+            responses.GET,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/traces/verifier_app_uuid/quick",
+            json={
+                "filters": [
+                    {"filterType": "ALL", "count": 12},
+                    {"filterType": "OPEN", "count": 0},
+                ]
+            },
+            match=[
+                self._header_matcher,
+                matchers.query_param_matcher(
+                    {"severities": "HIGH,CRITICAL", "appVersionTags": "123"}
+                ),
+            ],
+        )
+
+        out = io.StringIO()
+        with contextlib.redirect_stdout(out):
+            self._action.verify_application()
+        # it should log useful messages
+        self.assertIn(
+            "No matching job outcome policy, checking vulnerabilities against threshold...",
+            out.getvalue(),
+        )
+        self.assertIn("The vulnerability count is 0 (below threshold)", out.getvalue())
+
+    @responses.activate
+    def test_verify_application_with_no_job_outcome_policy_above_threshold(
+        self,
+    ):
+        responses.add(
+            responses.POST,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/securityChecks",
+            status=200,
+            json={"security_check": {"result": None}},
+            match=[
+                self._header_matcher,
+                matchers.json_params_matcher(
+                    {
+                        "application_id": "verifier_app_uuid",
+                        "security_check_filter": {
+                            "app_version_tags": ["123"],
+                            "query_by": "APP_VERSION_TAG",
+                        },
+                        "origin": "GitHub/Python",
+                    }
+                ),
+            ],
+        )
+
+        responses.add(
+            responses.GET,
+            "https://apptwo.contrastsecurity.com/api/ng/anOrgId/traces/verifier_app_uuid/quick",
+            json={
+                "filters": [
+                    {"filterType": "ALL", "count": 12},
+                    {"filterType": "OPEN", "count": 7},
+                ]
+            },
+            match=[
+                self._header_matcher,
+                matchers.query_param_matcher(
+                    {"severities": "HIGH,CRITICAL", "appVersionTags": "123"}
+                ),
+            ],
+        )
+
+        out = io.StringIO()
+        # it should quit
+        with self.assertRaises(SystemExit) as cm:
+            with contextlib.redirect_stdout(out):
+                self._action.verify_application()
+        # it should exit non-zero
+        self.assertEqual(cm.exception.code, 1)
+        # it should log useful messages
+        self.assertIn(
+            "No matching job outcome policy, checking vulnerabilities against threshold...",
+            out.getvalue(),
+        )
+        self.assertIn(
+            "The vulnerability count is 7 - Contrast verify gate fails as this is above threshold (threshold allows 0)",
+            out.getvalue(),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
--- verify.py
+++ verify.py
@@ -5,25 +5,25 @@ import requests
 from actions_toolkit import core as gh_action
 
 REQUIRED_INPUTS = ["CONTRAST_API_KEY", "CONTRAST_AUTHORIZATION", "CONTRAST_ORG_ID"]
-CONFIG = {}
 
 
 def validate_inputs():
     """Populate configuration object or set step to failed if required inputs are not set."""
     errors = []
+    config = {}
     for input in REQUIRED_INPUTS:
         val = gh_action.get_input(input)
         if val is None or val == "":
             errors.append(input)
         else:
-            CONFIG[input] = val
+            config[input] = val
 
     app_id = gh_action.get_input("APP_ID")
     app_name = gh_action.get_input("APP_NAME")
     if app_id and app_id != "":
-        CONFIG["APP_ID"] = app_id
+        config["APP_ID"] = app_id
     elif app_name and app_name != "":
-        CONFIG["APP_NAME"] = app_name
+        config["APP_NAME"] = app_name
     else:
         errors.append("(APP_ID or APP_NAME)")
 
@@ -34,144 +34,174 @@ def validate_inputs():
         gh_action.set_failed("Missing required inputs")
 
     host = gh_action.get_input("CONTRAST_HOST") or "app.contrastsecurity.com"
-    CONFIG["BASE_URL"] = f"https://{host}/Contrast/api/ng/{CONFIG['CONTRAST_ORG_ID']}/"
-    gh_action.debug(f'Base URL: {CONFIG["BASE_URL"]}')
+    config["BASE_URL"] = f"https://{host}/Contrast/api/ng/{config['CONTRAST_ORG_ID']}/"
+    gh_action.debug(f'Base URL: {config["BASE_URL"]}')
 
     severities = gh_action.get_input("SEVERITIES") or "CRITICAL,HIGH"
-    CONFIG["SEVERITIES"] = severities.upper()
+    config["SEVERITIES"] = severities.upper()
 
     fail_threshold = gh_action.get_input("FAIL_THRESHOLD") or 0
-    CONFIG["FAIL_THRESHOLD"] = int(fail_threshold)
+    config["FAIL_THRESHOLD"] = int(fail_threshold)
 
-    CONFIG["BUILD_NUMBER"] = gh_action.get_input("BUILD_NUMBER") or getenv("GITHUB_SHA")
+    config["BUILD_NUMBER"] = gh_action.get_input("BUILD_NUMBER") or getenv("GITHUB_SHA")
 
-
-def teamserver_headers():
-    """Generate common request headers for TeamServer."""
-    return {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Api-Key": CONFIG["CONTRAST_API_KEY"],
-        "Authorization": CONFIG["CONTRAST_AUTHORIZATION"],
-    }
+    return config
 
 
-def get_request(path, parameters={}):
-    """Send a GET request to TeamServer."""
-    gh_action.debug(f"GET {path} {parameters}")
-    response = requests.get(
-        CONFIG["BASE_URL"] + path, params=parameters, headers=teamserver_headers()
-    )
-    response.raise_for_status()
-    return response
+class ContrastVerifyAction:
+    def __init__(self, config) -> None:
+        self._app_id = config.get("APP_ID")
+        self._app_name = config.get("APP_NAME")
+        self._base_url = config["BASE_URL"]
+        self._build_number = config["BUILD_NUMBER"]
+        self._contrast_api_key = config["CONTRAST_API_KEY"]
+        self._contrast_authorization = config["CONTRAST_AUTHORIZATION"]
+        self._fail_threshold = config["FAIL_THRESHOLD"]
+        self._severities = config["SEVERITIES"]
+        self._headers = None
 
+    @property
+    def teamserver_headers(self):
+        """Generate common request headers for TeamServer."""
+        if not self._headers:
+            self._headers = {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Api-Key": self._contrast_api_key,
+                "Authorization": self._contrast_authorization,
+            }
+        return self._headers
 
-def post_request(path, body):
-    """Send a POST request to TeamServer."""
-    gh_action.debug(f"POST {path}")
-    response = requests.post(
-        CONFIG["BASE_URL"] + path, json=body, headers=teamserver_headers()
-    )
-    response.raise_for_status()
-    return response
+    @property
+    def app_id(self):
+        """Return the app ID if provided in config, else look it up based on app name and cache it."""
+        if self._app_id:
+            return self._app_id
 
+        self.determine_application_id()
+        return self._app_id
 
-def validate_connection():
-    """Determine if we can successfully make an API call, else mark the step failed."""
-    try:
-        response = get_request("profile/")
-        response.json()
-    except Exception as e:
-        gh_action.set_failed(str(e))
-
-
-def determine_application_id():
-    """Determine application ID from application name for config."""
-    if "APP_ID" in CONFIG:
-        gh_action.info(f'Using provided application ID {CONFIG["APP_ID"]}')
-        return
-
-    response = get_request(f'/applications/name?filterText={CONFIG["APP_NAME"]}')
-    applications = response.json()["applications"]
-    matching_named_apps = list(
-        filter(lambda app: app["name"] == CONFIG["APP_NAME"], applications)
-    )
-    if len(matching_named_apps) != 1:
-        gh_action.set_failed(
-            f'Could not match one app with name "{CONFIG["APP_NAME"]}", found {len(matching_named_apps)}, consider using APP_ID input instead.'
+    def get_request(self, path, parameters={}):
+        """Send a GET request to TeamServer."""
+        gh_action.debug(f"GET {path} {parameters}")
+        response = requests.get(
+            self._base_url + path, params=parameters, headers=self.teamserver_headers
         )
-    else:
-        CONFIG["APP_ID"] = matching_named_apps[0]["app_id"]
-        gh_action.info(f'Application ID for {CONFIG["APP_NAME"]} is {CONFIG["APP_ID"]}')
+        response.raise_for_status()
+        return response
 
+    def post_request(self, path, body):
+        """Send a POST request to TeamServer."""
+        gh_action.debug(f"POST {path}")
+        response = requests.post(
+            self._base_url + path, json=body, headers=self.teamserver_headers
+        )
+        response.raise_for_status()
+        return response
 
-def perform_security_check():
-    """Call the security check endpoint and return job outcome policy data."""
-    gh_action.info(f'Using app version tags: [{CONFIG["BUILD_NUMBER"]}]')
-    response = post_request(
-        "securityChecks",
-        {
-            "application_id": CONFIG["APP_ID"],
-            "security_check_filter": {
-                "query_by": "APP_VERSION_TAG",
-                "app_version_tags": [CONFIG["BUILD_NUMBER"]],
+    def validate_connection(self):
+        """Determine if we can successfully make an API call, else mark the step failed."""
+        try:
+            response = self.get_request("profile/")
+            response.json()
+        except Exception as e:
+            gh_action.set_failed(
+                f"Connection test failed, please verify credentials (agent credentials will not work) - {e}"
+            )
+
+    def determine_application_id(self):
+        """Determine application ID from application name for config."""
+        if self._app_id:
+            gh_action.info(f"Using provided application ID {self._app_id}")
+            return
+
+        response = self.get_request(f"applications/name?filterText={self._app_name}")
+        applications = response.json()["applications"]
+        matching_named_apps = list(
+            filter(lambda app: app["name"] == self._app_name, applications)
+        )
+        if len(matching_named_apps) != 1:
+            gh_action.set_failed(
+                f'Could not match one app with name "{self._app_name}", found {len(matching_named_apps)}, consider using APP_ID input instead.'
+            )
+        else:
+            self._app_id = matching_named_apps[0]["app_id"]
+            gh_action.info(f'Application ID for "{self._app_name}" is {self._app_id}')
+
+    def perform_security_check(self):
+        """Call the security check endpoint and return job outcome policy data."""
+        gh_action.info(f"Using app version tags: [{self._build_number}]")
+        response = self.post_request(
+            "securityChecks",
+            {
+                "application_id": self.app_id,
+                "security_check_filter": {
+                    "query_by": "APP_VERSION_TAG",
+                    "app_version_tags": [self._build_number],
+                },
+                "origin": "GitHub/Python",
             },
-            "origin": "GitHub/Python",
-        },
-    )
-    return response.json()
-
-
-def fetch_vulnerability_count():
-    """Call the vulnerability quick filter endpoint to return number of vulnerabilities."""
-    response = get_request(
-        f'traces/{CONFIG["APP_ID"]}/quick',
-        {
-            "severities": CONFIG["SEVERITIES"],
-            "appVersionTags": [CONFIG["BUILD_NUMBER"]],
-        },
-    )
-    return response.json()
-
-
-validate_inputs()
-validate_connection()
-determine_application_id()
-
-# First check for a configured job outcome policy defined in TeamServer
-job_outcome_policy_result = perform_security_check()
-gh_action.debug(job_outcome_policy_result)
-security_check_result = job_outcome_policy_result["security_check"]["result"]
-if security_check_result == False:
-    jop_policy = job_outcome_policy_result["security_check"]["job_outcome_policy"]
-    jop_outcome = jop_policy["outcome"]
-    jop_name = jop_policy["name"]
-    if jop_policy["opt_into_query"] == False:
-        gh_action.info(
-            f'Matching policy "{jop_name}" is not configured to apply the "query vulnerabilities by selection from the plugin when filtering vulnerabilities" option, this means all open vulnerabilities will be considered, not just those from the build_number input.'
         )
+        return response.json()
 
-    gh_action.set_failed(
-        f'Contrast verify gate fails with status {jop_outcome} - policy "{jop_name}"'
-    )
-elif security_check_result == True:
-    gh_action.info("Step passes matching policy")
-else:
-    # At this point, there is no matching job outcome policy in TeamServer, so query the open vulnerability count instead
-    gh_action.info(
-        "No matching job outcome policy, checking vulnerabilities against threshold..."
-    )
-    response = fetch_vulnerability_count()
-    gh_action.debug(response)
-    open_vulnerabilities_data = next(
-        filter(lambda filter: filter["filterType"] == "OPEN", response["filters"])
-    )
-    open_vulnerabilities = open_vulnerabilities_data["count"]
-    if open_vulnerabilities > CONFIG["FAIL_THRESHOLD"]:
-        gh_action.set_failed(
-            f'The vulnerability count is {open_vulnerabilities} - Contrast verify gate fails as this is above threshold (threshold allows {CONFIG["FAIL_THRESHOLD"]})'
+    def fetch_vulnerability_count(self):
+        """Call the vulnerability quick filter endpoint to return number of vulnerabilities."""
+        response = self.get_request(
+            f"traces/{self._app_id}/quick",
+            {
+                "severities": self._severities,
+                "appVersionTags": [self._build_number],
+            },
         )
-    else:
-        gh_action.info(
-            f"The vulnerability count is {open_vulnerabilities} (below threshold)"
-        )
+        return response.json()
+
+    def verify_application(self):
+        # First check for a configured job outcome policy defined in TeamServer
+        job_outcome_policy_result = self.perform_security_check()
+        gh_action.debug(job_outcome_policy_result)
+        security_check_result = job_outcome_policy_result["security_check"]["result"]
+        if security_check_result == False:
+            jop_policy = job_outcome_policy_result["security_check"][
+                "job_outcome_policy"
+            ]
+            jop_outcome = jop_policy["outcome"]
+            jop_name = jop_policy["name"]
+            if jop_policy["opt_into_query"] == False:
+                gh_action.info(
+                    f'Matching policy "{jop_name}" is not configured to apply the "query vulnerabilities by selection from the plugin when filtering vulnerabilities" option, this means all open vulnerabilities will be considered, not just those from the build_number input.'
+                )
+
+            gh_action.set_failed(
+                f'Contrast verify gate fails with status {jop_outcome} - policy "{jop_name}"'
+            )
+        elif security_check_result == True:
+            gh_action.info("Step passes matching policy")
+        else:
+            # At this point, there is no matching job outcome policy in TeamServer, so query the open vulnerability count instead
+            gh_action.info(
+                "No matching job outcome policy, checking vulnerabilities against threshold..."
+            )
+            response = self.fetch_vulnerability_count()
+            gh_action.debug(response)
+            open_vulnerabilities_data = next(
+                filter(
+                    lambda filter: filter["filterType"] == "OPEN", response["filters"]
+                )
+            )
+            open_vulnerabilities = open_vulnerabilities_data["count"]
+            if open_vulnerabilities > self._fail_threshold:
+                gh_action.set_failed(
+                    f"The vulnerability count is {open_vulnerabilities} - Contrast verify gate fails as this is above threshold (threshold allows {self._fail_threshold})"
+                )
+            else:
+                gh_action.info(
+                    f"The vulnerability count is {open_vulnerabilities} (below threshold)"
+                )
+
+
+if __name__ == "__main__":
+    config = validate_inputs()
+    action = ContrastVerifyAction(config)
+    action.validate_connection()
+    action.determine_application_id()
+    action.verify_application()
