a1de712a31d38505702cc85b4d76b6713d36ef6e
Josh Anderson <josh.anderson@contrastsecurity.com>
2022-05-19 16:52:33 +0100

    Add some initial test summary messages

--- verify.py
+++ verify.py
@@ -123,7 +123,10 @@ if __name__ == "__main__":
         output_helper,
         cert_file,
     )
+    action._output_helper.write_summary("# Contrast Verify")
     action.validate_connection()
     action.validate_organization()
+    action._output_helper.write_summary("- Connection Validated! :white_check_mark:")
     action.determine_application_id()
+    action._output_helper.write_summary("- Application found! :white_check_mark:")
     action.verify_application()
